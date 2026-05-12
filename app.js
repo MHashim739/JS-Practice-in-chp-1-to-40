4b36902ae5be93ed3351fe32cd2fdd40b1ba901a
Muhammad hashim <mh4165209@gmail.com>
2026-05-12 01:12:03 -0700

    for loop practice

--- app.js
+++ app.js
@@ -36,39 +36,67 @@
     
 // }
 
-function add(a,b,c){
-    return a + b + c ;
+// function add(a,b,c){
+//     return a + b + c ;
 
-}
-function mul(a,b,c){
-    return a * b * c ;
+// }
+// function mul(a,b,c){
+//     return a * b * c ;
     
-}
-function div(a,b,c){
-    return a / b / c ;
+// }
+// function div(a,b,c){
+//     return a / b / c ;
     
-}
-function sub(a,b,c){
-    return a - b - c ;
+// }
+// function sub(a,b,c){
+//     return a - b - c ;
     
-}
-var uInput1 = +prompt("Enter your first Num");
-var uInput2 = +prompt("Enter your second Num");
-var uInput3 = +prompt("Enter your Third Num");
-var Operaters = prompt("Choose Any Operators + , * , / , -");
-switch (Operaters){
-    case "+":
-        alert(add(uInput1,uInput2,uInput3));
-        break;
-    case "*":
-        alert (mul(uInput1,uInput2,uInput3));
-        break;
-    case "/":
-        alert(div(uInput1,uInput2,uInput3));
-        break;
-    case "-":
-       alert(sub(uInput1,uInput2,uInput3));
-       break;
-    default:
-        alert("Invalid! Operator");            
-}
+// }
+// var uInput1 = +prompt("Enter your first Num");
+// var uInput2 = +prompt("Enter your second Num");
+// var uInput3 = +prompt("Enter your Third Num");
+// var Operaters = prompt("Choose Any Operators + , * , / , -");
+// switch (Operaters){
+//     case "+":
+//         alert(add(uInput1,uInput2,uInput3));
+//         break;
+//     case "*":
+//         alert (mul(uInput1,uInput2,uInput3));
+//         break;
+//     case "/":
+//         alert(div(uInput1,uInput2,uInput3));
+//         break;
+//     case "-":
+//        alert(sub(uInput1,uInput2,uInput3));
+//        break;
+//     default:
+//         alert("Invalid! Operator");            
+// }
+
+
+// for(var i=1; i<4; i++){
+//     console.log("hello World!"+i);
+    
+// }
+// for(var j=1; j<=10; j++){
+//     console.log(j);
+    
+// }
+
+// var a=10;
+// a--;
+// a--;
+// a--;
+// console.log(a);
+
+// for(var i=10; i>=1; i--){
+//     console.log(i);
+    
+// }
+// for(var i=30; i>21; i++){
+//     console.log(i);
+    
+// }
+
+var monts = [1,2.2,3 ,"first","second",false ,12,"World"];
+console.log(typeof(monts[monts.length-1]));
